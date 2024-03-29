92c0743b3f8d93a1878fea7825425bb195024b71
KRUTIKA PATEL <krutika00.shrey8194@gmail.com>
2024-03-29 14:20:30 +0530

    df

---
+++ giftbankend/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import colors from "colors";
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect('mongodb://localhost:27017/mygift');
+    console.log(
+      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
+    );
+  } catch (error) {
+    console.log(`Errro in Mongodb ${error}`.bgRed.white);
+  }
+};
+
+export default connectDB;
+
+
+
+
+
